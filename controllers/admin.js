92cf6feb1dcb282a4568689abdfff60c0d2c1c1d
Petr <petr.kupriyanov@yandex.ru>
2021-02-08 16:41:22 +0300

    some message

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,14 +1,39 @@
 const Toy = require('../models/Toy')
 
-exports.create = async (req, res) => {
-  const {
-    title,
-    desc,
-    status
-  } = req.body
-  const files = req.files.map(img => '/' + img.path)
-  const toy = new Toy(title, desc, status, files)
-  await toy.save()
-  
-  res.status(201).render('admin/create', toy)
+exports.create = async(req, res) => {
+    const {
+        title,
+        desc,
+        status
+    } = req.body
+    const files = req.files.map(img => '/' + img.path)
+    const toy = new Toy(title, desc, status, files)
+    await toy.save()
+
+    res.status(201).render('admin/create', toy)
+}
+
+exports.delete = async(req, res) => {
+    const id = req.params.id
+    await Toy.deleteById(id)
+    res.status(200).render('admin/toys')
+}
+
+exports.edit = async(req, res) => {
+    const id = req.params.id
+    const {
+        title,
+        desc,
+        status,
+    } = req.body
+    const toy = {
+        title,
+        desc,
+        status
+    }
+
+    const files = req.files.map(img => '/' + img.path)
+    toy.images = files
+    await Toy.updateById(id, toy)
+    res.status(200).render('admin/toy', toy)
 }
--- router/admin.js
+++ router/admin.js
@@ -1,26 +1,28 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const {
-  create,
+    create,
+    delete,
+    edit
 } = require('../controllers/admin')
 const upload = require('../util/multer')
 
 const router = Router()
 
 router.all('*', (req, res, next) => {
-  const {pass} = req.query
-  if (pass === process.env.ADMIN_PASSWORD) {
-    res.locals.ADIMN_PASSWORD = pass
-    next()
-  } else {
-    res.send('<h1>Не пущу</h1>')
-  }
+    const { pass } = req.query
+    if (pass === process.env.ADIMN_PASSWORD) {
+        res.locals.ADIMN_PASSWORD = pass
+        next()
+    } else {
+        res.send('<h1>Не пущу</h1>')
+    }
 })
 
-router.delete('/:id')
+router.delete('/:id', delete)
 
 // see https://www.npmjs.com/package/multer documentation
-router.post('/', upload.array('images', 10), create)
+router.post('/', upload.array('images', 5), create)
 
-router.put('/:id')
+router.put('/:id', upload.array('images', 5), edit)
 
 module.exports = router
