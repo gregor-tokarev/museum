ccad1357906daa6686c7864430eef97facb0548d
Egor Tokarev <zippper123915@gmail.com>
2021-02-08 10:58:28 +0300

    add admin auth

--- router/admin.js
+++ router/admin.js
@@ -1,10 +1,20 @@
-const { Router } = require('express')
+const {Router} = require('express')
 const {
-  create
+  create,
 } = require('../controllers/admin')
 const upload = require('../util/multer')
 
 const router = Router()
+
+router.all('*', (req, res, next) => {
+  const {pass} = req.query
+  if (pass === process.env.ADMIN_PASSWORD) {
+    res.locals.ADIMN_PASSWORD = pass
+    next()
+  } else {
+    res.send('<h1>Не пущу</h1>')
+  }
+})
 
 router.delete('/:id')
 
