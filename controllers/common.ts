ef20107915b649a3107fa6c4dead6d829cf96c05
EgorT <zippper123915@gmail.com>
2021-03-18 11:06:05 +0300

    add toys connection with backend

--- controllers/common.ts
+++ controllers/common.ts
@@ -5,6 +5,7 @@ import Toy from "../models/Toy";
 export const search: RequestHandler = async (req, res) => {
   const query: string = req.query.search_query as string || ''
   const toys = await Toy.search(query)
+  
   res.status(200).render('index', {toys})
 }
 
