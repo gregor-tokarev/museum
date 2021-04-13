a515c23c6a84a12f4309b066444d98d843af619c
EgorT <zippper123915@gmail.com>
2021-04-13 17:06:42 +0300

    leading fix

---
+++ static/js/leanding.js
@@ -0,0 +1,21 @@
+const arrowEl = document.querySelector('#arrow')
+
+arrowEl.addEventListener('click', event => {
+    window.scrollBy({
+        top: 400,
+        behavior: 'smooth'
+    })
+})
+
+
+const header = document.querySelector('.index_header')
+window.addEventListener('scroll', event => {
+    const screenHeight = document.documentElement.clientHeight
+    const scrolled = document.documentElement.scrollTop
+    console.log(screenHeight, scrolled);
+    if (screenHeight - scrolled - 100 <= 0) {
+        header.classList.add('header--solid')
+    } else {
+        header.classList.remove('header--solid')
+    }
+})
