182eccdb4dfbf25662f1b071e6d8222ea922b542
Egor Tokarev <zippper123915@gmail.com>
2021-04-14 07:58:04 +0300

    make header adaptive

--- static/js/leanding.js
+++ static/js/leanding.js
@@ -1,21 +1,28 @@
 const arrowEl = document.querySelector('#arrow')
 
 arrowEl.addEventListener('click', event => {
-    window.scrollBy({
-        top: 400,
-        behavior: 'smooth'
-    })
+  window.scrollBy({
+    top: 400,
+    behavior: 'smooth',
+  })
 })
 
 
 const header = document.querySelector('.index_header')
 window.addEventListener('scroll', event => {
-    const screenHeight = document.documentElement.clientHeight
-    const scrolled = document.documentElement.scrollTop
-    console.log(screenHeight, scrolled);
-    if (screenHeight - scrolled - 100 <= 0) {
-        header.classList.add('header--solid')
-    } else {
-        header.classList.remove('header--solid')
-    }
+  const screenHeight = document.documentElement.clientHeight
+  const scrolled = document.documentElement.scrollTop
+  console.log(screenHeight, scrolled)
+  if (screenHeight - scrolled - 100 <= 0) {
+    header.classList.add('header--solid')
+  } else {
+    header.classList.remove('header--solid')
+  }
+})
+
+const openHeaderButton = document.querySelector('.open')
+
+openHeaderButton.addEventListener('click', event => {
+  console.log('some')
+  header.classList.toggle('header--open')
 })
